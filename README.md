cb338edfbf1f5f4518f44ca014e035d9e6b4829e
Michael Bryan <michaelfbryan@gmail.com>
2016-09-19 19:37:16 +0800

    Updated CAN to the newest driver version

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-CMT Electrical Libraries
-========================
+README
+======
 
 A general repository to keep track of the various libraries CMT use in
 their electrical projects.
@@ -7,6 +7,8 @@ their electrical projects.
 
 Documentation
 -------------
+
+### Building ###
 
 To automatically generate the documentation, use:
 
@@ -17,3 +19,94 @@ point your browser at the documentation home page (`docs/html/index.html`),
 you'll be able to explore the entire repository. You can explore the
 file tree to find the signatures and documentation for pretty much
 everything.
+
+(Note: You need to have [Doxygen][doxygen] installed to build docs)
+
+
+### Writing ###
+
+If you want `Doxygen` to automatically generate documentation from your
+source files, you need to format your comments slightly differently.
+
+Often this is just a case of adding a `!` after the `/*` or `//`
+when you write a comment.
+
+
+    //!  A test class.
+    /*!
+     *  A more elaborate class description.
+     */
+    class QTstyle_Test
+    {
+    public:
+        //! An enum.
+        /*! More detailed enum description. */
+        enum TEnum {
+                    TVal1, /*!< Enum value TVal1. */
+                    TVal2, /*!< Enum value TVal2. */
+                    TVal3  /*!< Enum value TVal3. */
+                }
+            //! Enum pointer.
+            /*! Details. */
+            *enumPtr,
+            //! Enum variable.
+            /*! Details. */
+            enumVar;
+
+        //! A constructor.
+        /*!
+         * A more elaborate description of the constructor.
+         */
+        QTstyle_Test();
+        //! A destructor.
+        /*!
+         * A more elaborate description of the destructor.
+         */
+    ~QTstyle_Test();
+
+        //! A normal member taking two arguments and returning an integer value.
+        /*!
+         * \param a an integer argument.
+         * \param s a constant character pointer.
+         * \return The test results
+         * \sa QTstyle_Test(), ~QTstyle_Test(), testMeToo() and publicVar()
+         */
+        int testMe(int a,const char *s);
+
+        //! A pure virtual member.
+        /*!
+         * \sa testMe()
+         * \param c1 the first argument.
+         * \param c2 the second argument.
+         */
+        virtual void testMeToo(char c1,char c2) = 0;
+
+        //! A public variable.
+        /*!
+         * Details.
+         */
+        int publicVar;
+
+        //! A function variable.
+        /*!
+         * Details.
+         */
+        int (*handler)(int a,int b);
+    };
+
+
+Contributing
+------------
+
+To make any changes to this repository, first create your own fork (using
+the `Fork` button in the top left when you look at the repo on GitHub), make
+your changes, then send a pull request so the changes can be merged back into
+the main repository.
+
+For more information on GitHub's [Fork and Pull Request][fork-pull-request]
+model check the docs.
+
+
+
+[doxygen]: http://www.stack.nl/~dimitri/doxygen/download.html
+[fork-pull-request]: https://help.github.com/articles/about-pull-requests/
--- src/can/CAN.c
+++ src/can/CAN.c
@@ -1,21 +1,4 @@
-/*
-CODE: CAN COMMS LIBRARY
-AUTHOR: John Mai, Thomas Paynter and Jack Evans
-COMPANY:CURTIN MOTORSPORT TEAM
-LAST EDITED: 30/11/15
-TECH DETAILS:CAN communications over ID's 1 and 2 at a baud rate of 1Mbps.
-*/
-
-/*
-LIBRARY DESCRIPTION:
- * This library allows for the PIC18F46K80 and MCCP2551 CAN hardware node to com-
- * municate with the CAN bus on the CMT '15 vehicle. Primarily it will be linked 
- * with the LINK G4+ ECU CAN bus node. The library functions allow for basic com-
- * munication functionalities such as transmitting, receiving and also initialis-
- * ation of the node itself.
-*/
-
-#include<pic18f46k80.h>
+#include <pic18f46k80.h>
 #include <xc.h>
 #include "CAN.h"
 
@@ -24,7 +7,7 @@ extern unsigned char data1[8], data2[8];
 
 void CAN_Initialise()
 {
-    
+
     /*CAN needs to be in CONFIG mode to edit the following settings.
     * We do that by editing CANCONbits.REQOP (Bits 7-5).*/
     CANCON = 0b10000000;
@@ -39,28 +22,28 @@ void CAN_Initialise()
     * enhanced legacy mode mode is basically CAN, but a little bit better
     * Here we set the module to mode 1, enhanced legacy.*/
     ECANCON = 0b01000000;
-    
-    // Initialize CAN Timing  
+
+    // Initialize CAN Timing
     //  Setting Baud Rate
-    //  125 Kbps @ 16MHz 
-    
+    //  125 Kbps @ 16MHz
+
     //BRGCON1
-    
+
     BRGCON1bits.SJW = 0b00; // Sync Jump Width is 1 TQ
     BRGCON1bits.BRP = 0; // Baud Rate Prescaler is Tq = 2*(BRP + 1)/Fosc = 0.125 us
-    
-    
+
+
     //BRGCON2
     BRGCON2bits.SEG2PHTS = 1; // Makes the SEG2 fully programmable
     BRGCON2bits.SAM = 0; // On reception the bit is sampled once at the intersection of SEG1 and SEG2
     BRGCON2bits.SEG1PH = 0b011; // SEG1 is 4*TQ
     BRGCON2bits.PRSEG = 0b000; // Propogation time is 1*TQ
-    
+
     //BREGCON3
     BRGCON3bits.WAKDIS = 0; //Enable wakeup from CAN
     BRGCON3bits.WAKFIL = 0; //Disable filters for wakeup
     BRGCON3bits.SEG2PH = 0b001; //SEG2 is 2*TQ
-    
+
     // Thus the timeline for the CAN bit is:
     //  1           1   4       2   = 8 TQ = 1 MBits/s
     // SyncSeg  PropSeg SEG1    SEG2
@@ -68,26 +51,26 @@ void CAN_Initialise()
     BSEL0 = 0b11111100;                   //1=TX, 0=RX
 
 	/* A bit on Masks and Filters!
-			
-	A filter is the be all and end all, it decides to accept the data to the buffer or 
+
+	A filter is the be all and end all, it decides to accept the data to the buffer or
 	dump it. A filter will be composed of a bit field as long as the ID of the CAN message.
-	
-	If the CAN ID matches the filter it is let throgut to the buffer. 
-	
-	Now the mask is like a wild	card to the filter. If the corresponding mask bit is a 
+
+	If the CAN ID matches the filter it is let throgut to the buffer.
+
+	Now the mask is like a wild	card to the filter. If the corresponding mask bit is a
 	1 then the filter bit MUST match the corresponding ID bit in order for it
 	to be passed. If its a 0 then it is a dont care bit. ie
-	
+
 	Mask = 	0b01101101
 	Filter=	0b11011011
-	Passes=	0bx10x10x1 
+	Passes=	0bx10x10x1
 	  ie	0b11011011
 			0b01011011
 			0b11001001 ect
-	
+
 	Think of the mask as a filter bit enable function,  if it is a 0 it disables that filter
 	checking ability*/
-	
+
     /* Initialize Receive Masks
     The first mask is used that accepts all SIDs */
     RXM0SIDH = 0b11111111; // Standard ID MASK all 1s, meaning the filter must match EXACTLY!
@@ -107,7 +90,7 @@ void CAN_Initialise()
     // Assign Filters to Buffers
     RXFBCON0 = 0b00010000;               //Assign Filter 0 to RXB0, and Filter 1 to RXB1
     RXFBCON1 = 0b11111111;                     //Assign the rest of the buffers with no filter
-    RXFBCON2 = 0b11111111;                     
+    RXFBCON2 = 0b11111111;
     RXFBCON3 = 0b11111111;
     RXFBCON4 = 0b11111111;
     RXFBCON5 = 0b11111111;
@@ -117,10 +100,10 @@ void CAN_Initialise()
     // Initialize Receive Filters
     RXF0SIDH = 0b00000000; // Set to ID1
     RXF0SIDL = 0b00100000;
-    
+
     RXF1SIDH = 0b00000000; // Set to ID2
     RXF1SIDL = 0b01000000;
-    
+
     /* In this case, we are only passing messages with ID=1 and 2 through.
     The SID has been split between  two registers,
     bits SID<10:3> lie in RXFnSIDH and SID<2:0> are in MSBs of RXFnSIDL.
@@ -140,44 +123,44 @@ void CAN_Transmit(unsigned char data[8], unsigned int SID, unsigned char DLC)
 {
    // We've left out the setup for extended ID transmit buffers, not used for sending standard ID messages, so currently not necessary.
     while(TXB0CONbits.TXREQ == 1); //Wait for last message to clear
-    
+
 /* Standard length ID transmit buffers, the actual identifier lies over the
 * two registers, bits SID<10:3> lie in TXBnSIDH and SID<2:0> are in MSBs of
-* TXBnSIDL. The TXBnSIDLbits.EXIDE, bit 3 bit should be 0 or 1 for 
+* TXBnSIDL. The TXBnSIDLbits.EXIDE, bit 3 bit should be 0 or 1 for
 * standard/extended IDs, respectively. The remainder of TXBnSIDL should be
 * set as 0 for standard IDs */
 TXB0SIDH = (SID >> 3) & 0b11111111;                           //Thus the SID is 00110010 110 = 406
 TXB0SIDL = (SID << 5) & 0b11100000;
- 
+
 /*   The TXBnDLCbits.TXRTR (bit 6) is the remote transmission request bit, and
-      tells another node of that ID to send data. 
+      tells another node of that ID to send data.
       The TXBnDLCbits.DLC (bits 3:0) is an indication of how many bytes are to be sent, it can range from 0 to 8   */
 
 TXB0DLC = DLC;                            //Thus DLC bytes will be sent! (This is the max)
- 
+
 /*  This is where the meat of the CAN it, this is it!!!!!
 The TXBnDm register is the actual data you are to be sending over the
 network  */
-TXB0D0 = data[0];            
-TXB0D1 = data[1];                  
-TXB0D2 = data[2];                  
-TXB0D3 = data[3];                 
-TXB0D4 = data[4];                
-TXB0D5 = data[5];       
-TXB0D6 = data[6];       
-TXB0D7 = data[7];       
+TXB0D0 = data[0];
+TXB0D1 = data[1];
+TXB0D2 = data[2];
+TXB0D3 = data[3];
+TXB0D4 = data[4];
+TXB0D5 = data[5];
+TXB0D6 = data[6];
+TXB0D7 = data[7];
 
 /*This bit literally says go for transmit!!*/
 TXB0CONbits.TXREQ = 1;                     //Set the buffer to transmit
- 
+
 }
 
 void CAN_Receive(unsigned char ID)
 {
     // Checks if there is data in buffer number [buffer].
     // If there is then it copies register RXBn into the array data1[8]
-    
-    
+
+
         if ((RXB0CONbits.RXFUL) && (ID == 1) )          // receive buffer 0
             {
             data1[0] = RXB0D0;
@@ -194,7 +177,7 @@ void CAN_Receive(unsigned char ID)
             }
 
         if ((RXB1CONbits.RXFUL) && (ID == 2) )           // receive buffer 1
-        {                   
+        {
                 data2[0] = RXB1D0;
                 data2[1] = RXB1D1;
                 data2[2] = RXB1D2;
@@ -208,5 +191,5 @@ void CAN_Receive(unsigned char ID)
                 PIR5bits.RXB1IF = 0;     // reset receive buffer 1 interrupt flag
         }
 
-  
+
 }
